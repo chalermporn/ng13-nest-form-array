5b5d40e2318cbb75061df2b2da97599eb02748f0
Sutin <poly.dev2018@gmail.com>
2022-06-06 00:42:56 +0700

    feat: added nest form array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Form, FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng13-form-array';
+  myCourse: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myCourse = this.fb.group({
+      myCourseLists: this.fb.array([this.addNewCourse(), this.addNewCourse(), this.addNewCourse()])
+    })
+
+    console.log(this.myCourse);
+  }
+
+  get myCourseLists(): FormArray {
+    return this.myCourse.get('myCourseLists') as FormArray;
+  }
+  courseLists(index: number): FormArray {
+    const c1 = this.myCourseLists.at(index)
+    return c1?.get('courseLists') as FormArray;
+  }
+  studies(index: number): FormArray {
+    const c2 = this.courseLists(index).at(index)
+    return c2?.get('studies') as FormArray;
+  }
+
+  addNewCourse(): FormGroup {
+    return this.fb.group({
+      name: ['C1'],
+      courseLists: this.fb.array([this.addNewCourseList()])
+    })
+
+  }
+
+  addNewCourseList(): FormGroup {
+    return this.fb.group({
+      name: ['C2'],
+      point: [1],
+      studies: this.fb.array([this.addNewStudyList()])
+    })
+  }
+
+  addNewStudyList(): FormGroup {
+    return this.fb.group({
+      name: ['C3'],
+      class: 'Y1'
+    })
+  }
 }
